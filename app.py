45d7abd9c4e2442bef041cc25533fff8dca1c1d0
진정현 <https://github.com/cvdf3333/Navigator.git>
2026-06-10 20:17:21 +0900

    포트 3000으로 변경

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import sys
 import os
 
-# 경로 추가
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "backend"))
 
 from backend.app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000)
